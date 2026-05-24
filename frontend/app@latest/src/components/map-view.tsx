76440b12250b7dca275fa532f8920b3b96de31a4
sthaarwin <sthaarwin123@gmail.com>
2026-05-24 17:57:00 +0545

    Add GPS current location to live map

---
+++ frontend/app@latest/src/components/map-view.tsx
@@ -0,0 +1,5 @@
+import type MapViewType from 'react-native-maps';
+import type { Marker as MarkerType } from 'react-native-maps';
+
+export const MapView: typeof MapViewType = null as any;
+export const Marker: typeof MarkerType = null as any;
